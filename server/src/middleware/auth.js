b44601d55122f84525c59c86a528b97f61699a14
Zaki Ayoubi <zakihorakhsh2028@gmail.com>
2026-02-20 14:28:24 -0800

    delete the backend to rewrite in python

--- server/src/middleware/auth.js
+++
@@ -1,5 +0,0 @@
-// src/middleware/auth.js
-export function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) return next();
-  res.status(401).json({ success: false, message: "Not authenticated" });
-}
