470f28d8f197ff616adace13b7d747bae4e0b0bd
Zaki Ayoubi <zakihorakhsh2028@gmail.com>
2026-02-23 20:33:33 -0800

    Implement get tasks endpoint with filtering

--- backend/app/routes/tasks.py
+++ backend/app/routes/tasks.py
@@ -5,6 +5,8 @@ from app.models.user import User
 from app.utils.auth import get_current_user
 from app.database import get_db
 from app.models.task import Task
+from typing import Optional
+from datetime import date
 
 router = APIRouter(prefix="/tasks", tags=["Tasks"])
 
@@ -35,3 +37,35 @@ def create_task(
     db.refresh(new_task)
 
     return new_task
+
+
+@router.get(
+    "/", response_model=list[TaskResponse], status_code=status.HTTP_200_OK
+)
+def get_task(
+    status: Optional[bool] = None,
+    due_date: Optional[date] = None,
+    priority: Optional[bool] = None,
+    current_user: User = Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
+    query = db.query(Task).filter(Task.user_id == current_user.id)
+
+    if status is not None:
+        query = query.filter(Task.is_completed == status)
+
+    # status = true means completed tasks
+    # status = false means active/incompelete tasks
+
+    if due_date:
+        query = query.filter(Task.due_date == due_date)
+
+    if priority is not None:
+        query = query.filter(Task.priority == priority)
+
+    # priority = true means high priority task
+    # priority = false means low priority task
+
+    # Order by due_date (most recent first), then created_at
+    tasks = query.order_by(Task.due_date.desc(), Task.created_at.desc()).all()
+    return tasks
